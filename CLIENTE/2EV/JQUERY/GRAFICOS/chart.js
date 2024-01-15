a6d89995cd7e14faa1d010ea633c6fd7ddd476af
Sofia Villarejo <zx22student3329@colegiovalledelmiro.es>
2024-01-15 13:57:53 +0100

    15/01

--- CLIENTE/2EV/JQUERY/GRAFICOS/chart.js
+++ CLIENTE/2EV/JQUERY/GRAFICOS/chart.js
@@ -1,136 +1,101 @@
-const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-const yValues = [55, 49, 44, 24, 15];
-const barColors = ["red", "green","blue","orange","brown"];
+$(inicio);
+// PARA EL EXAMEN ENTRAN --> BAR CHART, PIE CHART, LINE CHART Y SCATTER CHART
 
-new Chart("myChart", {
-  type: "bar",
-  data: {
-    labels: xValues,
+function inicio(){
+  const cnv = $("#misBarras");
+
+  const datos = {
+    labels:['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'],
+    datasets:[
+      //PRIMER CONJUNTO
+        {
+        label : "DATOS AZUL",
+        data:[1,2,3,4,2,1,3],
+        borderWidth: 3,
+        backgroundColor: ["blue", "black"], //el primer valor, azul, el segundo negro y asi DE ESTE CONJUNTO DE DATOS (PRIMERA BARRA EN CADA CONJUNTO)
+        borderColor: ["yellow", "black"],
+        hoverBackgroundColor : ["white", "green"]
+        },
+      //SEGUNDO CONJUNTO
+        {
+        label : "DATOS ROSA",
+        data:[3,1,2,3,4,2,1],
+        backgroundColor: "pink"
+
+        },
+      //TERCER CONJUNTO
+        {
+        label : "DATOS AMARILLO",
+        data:[1,3,1,2,3,4,2],
+        backgroundColor: "yellow"
+
+        }
+      ]
+  };
+
+new Chart(cnv, {
+type: 'bar',
+  data: datos,
+  //poner barras en horizontal
+  options:{ 
+    indexAxis: 'y'
+  }
+});
+
+  const cnv2 = $("#miScatter");
+  const datos2 = {
     datasets: [{
-      backgroundColor: barColors,
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    title: {
-      display: true,
-      text: "World Wine Production 2018"
-    }
-  }
-});
+      label: 'Grafico Scatter',
+      data: [{
+        x: -10,
+        y: 0
+      }, 
+      {
+        x: 0,
+        y: 10
+      }, 
+      {
+        x: 10,
+        y: 5
+      }, 
+      {
+        x: 0.5,
+        y: 5.5
+      }],
+      backgroundColor: 'blue'
+    }],
+  };
 
-new Chart("myPieChart", {
-  type: "pie",
-  data: {
-    labels: xValues,
+  new Chart(cnv2, {
+    type: 'scatter',
+      data: datos2,
+    });
+
+  const cnv3 = $("#miPie");
+  const datos3 = {
+    labels: [
+      'Red',
+      'Blue',
+      'Yellow'
+    ],
     datasets: [{
-      backgroundColor: barColors,
-      data: yValues
+      label: 'Grafico de tarta',
+      data: [300, 50, 100],
+      backgroundColor: [
+        'red',
+        'blue',
+        'yellow'
+      ],
+      hoverOffset: 4
     }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: "World Wide Wine Production 2018"
-    }
-  }
-});
+  };
 
-new Chart("myDouChart", {
-    type: "doughnut",
-    data: {
-      labels: xValues,
-      datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-      }]
-    },
-    options: {
-      title: {
-        display: true,
-        text: "World Wide Wine Production 2018"
-      }
-    }
-  });
+  new Chart(cnv3, {
+    type: 'pie',
+      data: datos3,
+    });
 
-  const xyValues = [
-    {x:50, y:7},
-    {x:60, y:8},
-    {x:70, y:8},
-    {x:80, y:9},
-    {x:90, y:9},
-    {x:100, y:9},
-    {x:110, y:10},
-    {x:120, y:11},
-    {x:130, y:14},
-    {x:140, y:14},
-    {x:150, y:15}
-  ];
-  
-  new Chart("myScChart", {
-    type: "scatter",
-    data: {
-      datasets: [{
-        pointRadius: 4,
-        pointBackgroundColor: "rgb(0,0,255)",
-        data: xyValues
-      }]
-    },
-    options: {
-      legend: {display: false},
-      scales: {
-        xAxes: [{ticks: {min: 40, max:160}}],
-        yAxes: [{ticks: {min: 6, max:16}}],
-      }
-    }
-  });
 
-  
-  const xValores = [50,60,70,80,90,100,110,120,130,140,150];
-  const yValores = [7,8,8,9,9,9,10,11,14,14,15];
-  
-  new Chart("myLineChart", {
-    type: "line",
-    data: {
-      labels: xValores,
-      datasets: [{
-        fill: false,
-        lineTension: 0,
-        backgroundColor: "rgba(0,0,255,1.0)",
-        borderColor: "rgba(0,0,255,0.1)",
-        data: yValores
-      }]
-    },
-    options: {
-      legend: {display: false},
-      scales: {
-        yAxes: [{ticks: {min: 6, max:16}}],
-      }
-    }
-  });
 
-  const equix = [100,200,300,400,500,600,700,800,900,1000];
+}
 
-new Chart("myLineChart2", {
-  type: "line",
-  data: {
-    labels: equix,
-    datasets: [{ 
-      data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
-      borderColor: "red",
-      fill: false
-    }, { 
-      data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-      borderColor: "green",
-      fill: false
-    }, { 
-      data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-      borderColor: "blue",
-      fill: false
-    }]
-  },
-  options: {
-    legend: {display: false}
-  }
-});
